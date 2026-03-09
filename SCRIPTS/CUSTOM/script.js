639506269f38dba5eb2f16bcd3440d16bfd396c5
Dannyk24 <teckydan@gmail.com>
2026-03-09 21:39:22 +0100

    Built filters and implemented flatpickr

--- SCRIPTS/CUSTOM/script.js
+++ SCRIPTS/CUSTOM/script.js
@@ -11,3 +11,23 @@ navigationPanel.addEventListener('click',(e)=>{
     setActiveTab(tabName)
 })
 setActiveTab(getActiveTab()) /*uses session storage to return the last active tab when a user refreshes(persistence) or returns 'dashboard' on page load*/
+
+const calendar = flatpickr('#date-range-input',{
+    mode:'range',
+    dateFormat: "Y-m-d",
+    maxDate:'today',
+    altInput:true,
+    altFormat:'M-d',
+})
+
+const clearCalendarBtn = document.querySelector('.clear-calendar-btn')
+const calendarContainer = document.getElementById('date-range-container')
+calendarContainer.addEventListener('click',()=>{
+    calendar.open()
+})
+clearCalendarBtn.addEventListener('click',()=>{
+    calendar.clear()
+})
+
+
+
