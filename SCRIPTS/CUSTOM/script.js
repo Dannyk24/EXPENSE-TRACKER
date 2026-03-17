808572531c1f8abe96c6e2e9b846919ed090c796
Dannyk24 <teckydan@gmail.com>
2026-03-17 15:59:18 +0100

    Added js to user profile section

--- SCRIPTS/CUSTOM/script.js
+++ SCRIPTS/CUSTOM/script.js
@@ -28,3 +28,79 @@ calendarContainer.addEventListener('click',()=>{
 clearCalendarBtn.addEventListener('click',()=>{
     calendar.clear()
 })
+
+/*User Profile Script*/
+const submitFormDataBtn = document.querySelector('.save-form-changes')
+
+const formInputs = document.querySelectorAll('.personal-information-form-input')
+formInputs.forEach(input=>{
+    input.addEventListener('input',()=>{
+        if(input.value!== ''){
+            submitFormDataBtn.classList.add('third-button-active')
+            return
+        }
+        submitFormDataBtn.classList.remove('third-button-active')
+    })
+})
+
+submitFormDataBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    if(!submitFormDataBtn.classList.contains('third-button-active')){
+        return
+    }
+    const username = document.querySelector('#user-name-input').value
+    const email = document.querySelector('#user-email-input').value
+    const phone = document.querySelector('#user-phone-input').value
+    const user = {
+        username,
+        email,
+        phone
+    }
+    localStorage.setItem('user',JSON.stringify(user))
+    notify('success','User profile updated')
+    renderUserProfile()
+})
+
+
+let notifyTimeoutInterval;
+function notify(type,notifyMessage){
+    let toast;
+    if(type === 'success'){
+        toast = document.querySelector('.success-toast')
+    }
+    else if(type === 'danger'){
+        toast = document.querySelector('.danger-toast')
+    }
+    toast.querySelector('.toast-message').textContent = notifyMessage
+    toast.classList.add('toast-active')
+
+    clearTimeout(notifyTimeoutInterval)
+    notifyTimeoutInterval = setTimeout(()=>{
+        toast.classList.remove('toast-active')
+    },3500)
+}
+
+function renderUserProfile(){
+    const user = JSON.parse(localStorage.getItem('user')) || {
+        username:'John Doe',
+        email: 'john@example.com',
+        phone: '09013151297'
+    }
+    /*Profile section elements*/
+    const userProfileNameElem = document.querySelector('.user-profile-username')
+    const userProfileEmailElem = document.querySelector('.user-profile-user-email')
+    userProfileNameElem.textContent = user.username
+    userProfileEmailElem.textContent = user.email
+
+    /*Sidebar Elements*/
+    const sidebarUsername = document.querySelector('.sidebar-username')
+    const sidebarEmail = document.querySelector('.sidebar-email')
+    sidebarUsername.textContent = user.username
+    sidebarEmail.textContent = user.email
+
+    /*Change input field values to user profile values*/
+    document.querySelector('#user-name-input').value = user.username
+    document.querySelector('#user-email-input').value = user.email
+    document.querySelector('#user-phone-input').value  = user.phone
+}
+renderUserProfile()
